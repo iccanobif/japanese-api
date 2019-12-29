f71aae216cfa6039cc73c05623edab07faac7363
iccanobif <iccanobif@420blaze.it>
2019-12-29 13:42:08 +0100

    minimal web server

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const PORT = 8081
+
+const express = require("express")
+const app = express()
+const http = require("http").Server(app)
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    res.type("text/plain")
+    res.end("welcome")
+})
+
+http.listen(PORT, "0.0.0.0")
+console.log("Server running on port " + PORT)
