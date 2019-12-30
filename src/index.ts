1d88469179b0c9ee42c8236d96578e9b08586241
iccanobif <iccanobif@420blaze.it>
2019-12-30 19:25:07 +0100

    build with typescript

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get("/", (req: any, res: any) =>
 {
   res.type("text/plain")
-  res.end("welcome 2")
+  res.end("welcome 6")
 })
 app.get("/edict/:word", (req: any, res: any) => {
   const word = req.params.word
