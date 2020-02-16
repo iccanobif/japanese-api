81fd060c3d4446468d92f04ee74b857ce3a766c6
iccanobif <iccanobif@420blaze.it>
2020-02-16 17:06:03 +0100

    dictionary endpoint only returns relevant information

--- src/edict/repository.ts
+++ src/edict/repository.ts
@@ -1,11 +1,12 @@
 import { DictionaryEntry } from "../types";
 import { doOnMongoCollection } from "../utils";
 
-
 export async function getDictionaryEntries(query: string): Promise<DictionaryEntry[]> {
 
-  return await doOnMongoCollection<DictionaryEntry, DictionaryEntry[]>("dictionary",
-    async (coll) => {
-      return await coll.find({ keys: query }).toArray()
-    })
+  return await doOnMongoCollection<DictionaryEntry, any[]>("dictionary",
+    coll => coll
+      .find({ keys: query },
+        { projection: { glosses: 1, _id: 0 } })
+      .toArray()
+  )
 }
