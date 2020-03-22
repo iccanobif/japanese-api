f95414c43e4feca3c243f21bfa275b225492e27a
iccanobif <iccanobif@fake.mail>
2020-03-22 13:22:04 +0100

    disable CORS

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,20 @@ import { getDictionaryEntries } from "./edict/repository";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req: any, res: any) => {
+app.use((req, res, next) =>
+{
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  next()
+})
+
+app.get("/", (req: any, res: any) =>
+{
   res.type("text/plain")
   res.end("home")
 })
 
-app.get("/dictionary/:query", async (req: any, res: any) => {
+app.get("/dictionary/:query", async (req: any, res: any) =>
+{
   const query = req.params.query
   const entries = await getDictionaryEntries(query)
   res.json(entries)
