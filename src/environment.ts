95ce378cbed4edab9df359ad94c6c0dc0ce1dc13
iccanobif <iccanobif@fake.mail>
2020-04-13 19:19:53 +0200

    Get port number from heroku dev variables

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  httpPort: 8085,
+  httpPort: process.env.PORT || 8085,
   mongodbUrl: process.env.MONGODB_URI || "mongodb://localhost:27017",
   mongodbName: "japaneseapi"
 };
