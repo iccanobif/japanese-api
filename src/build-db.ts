425db085d387d0ecf67163877c70f0187cde3884
iccanobif <iccanobif@fake.mail>
2020-04-25 11:34:53 +0200

    use bulkWrite to speed database seeding up

--- src/build-db.ts
+++ src/build-db.ts
@@ -5,7 +5,8 @@ import { log, printError } from "./utils";
 import { DictionaryEntryInDb, Lemma } from "./types";
 import { daijirinReadIntermediateFile } from "./daijirin/scan-intermediate-file";
 
-const INSERT_BUFFER_LENGTH = 10000
+const EDICT_INSERT_BUFFER_LENGTH = 10000
+const DAIJIRIN_UPSERT_BUFFER_LENGTH = 8000
 
 export async function buildEdictDB()
 {
@@ -57,8 +58,9 @@ export async function buildEdictDB()
               .filter(l => !l.isConjugated)
               .map(l => l.reading)),
         })
-        if (insertBuffer.length == INSERT_BUFFER_LENGTH)
+        if (insertBuffer.length == EDICT_INSERT_BUFFER_LENGTH)
         {
+          log("Bulk writing edict")
           await dictionary.insertMany(insertBuffer)
           insertBuffer = []
         }
@@ -69,55 +71,66 @@ export async function buildEdictDB()
     await dictionary.createIndex({ allUnconjugatedKeys: 1 })
 
     log("Parsing daijirin...")
-    let daijirinCount = 0;
-    for await (const daijirinItem of daijirinReadIntermediateFile())
     {
-      daijirinCount++
-      if (daijirinCount % 10000 == 0)
-        console.log(daijirinCount)
+      let operationBuffer: any[] = []
 
-      // Find element in edict
-      const edictDocuments = await dictionary.find({
-        allUnconjugatedKeys: { $all: daijirinItem.keys }
-      }).toArray()
+      for await (const daijirinItem of daijirinReadIntermediateFile())
+      {
+        // Find element in edict
+        const edictDocuments = await dictionary.find({
+          allUnconjugatedKeys: { $all: daijirinItem.keys }
+        }).toArray()
 
-      if (edictDocuments.length == 0)
-      {
-        // Not in edict, insert to dictionary as a daijirin only document
-        await dictionary.insertOne({
-          lemmas: daijirinItem.keys.map((k: string): Lemma => ({
-            kanji: k,
-            reading: k,
-            isConjugated: false,
-          })),
-          edictGlosses: [],
-          allKeys: daijirinItem.keys,
-          allUnconjugatedKeys: daijirinItem.keys,
-          allConjugatedKeys: [],
-          daijirinArticles: [{
-            glosses: daijirinItem.glosses,
-            lemma: daijirinItem.lemma,
-          }]
-        })
-      }
-      else
-      {
-        // Caso strano: 口上
-        for (const edictDocument of edictDocuments)
+        if (edictDocuments.length == 0)
         {
-          await dictionary.updateOne({ _id: edictDocument._id },
-            {
-              $push:
-              {
-                daijirinArticles: {
-                  glosses: daijirinItem.glosses,
-                  lemma: daijirinItem.lemma,
+          // Not in edict, insert to dictionary as a daijirin only document
+          operationBuffer.push({
+            insertOne: {
+              lemmas: daijirinItem.keys.map((k: string): Lemma => ({
+                kanji: k,
+                reading: k,
+                isConjugated: false,
+              })),
+              edictGlosses: [],
+              allKeys: daijirinItem.keys,
+              allUnconjugatedKeys: daijirinItem.keys,
+              allConjugatedKeys: [],
+              daijirinArticles: [{
+                glosses: daijirinItem.glosses,
+                lemma: daijirinItem.lemma,
+              }]
+            }
+          })
+        }
+        else
+        {
+          // Caso strano: 口上
+          for (const edictDocument of edictDocuments)
+          {
+            operationBuffer.push({
+              updateOne: {
+                filter: { _id: edictDocument._id },
+                update: {
+                  $push:
+                  {
+                    daijirinArticles: {
+                      glosses: daijirinItem.glosses,
+                      lemma: daijirinItem.lemma,
+                    }
+                  }
                 }
               }
             })
+          }
+        }
+
+        if (operationBuffer.length >= DAIJIRIN_UPSERT_BUFFER_LENGTH)
+        {
+          log("Bulk writing daijirin")
+          await dictionary.bulkWrite(operationBuffer)
+          operationBuffer = []
         }
       }
-
     }
     log("Creating allKeys index on dictionary...")
     await dictionary.createIndex({ allKeys: 1 })
