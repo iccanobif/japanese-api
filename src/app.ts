e5151eb8c44295bb5e841b5063a6b471cd3c5468
iccanobif <iccanobif@fake.mail>
2020-05-12 22:16:49 +0200

    integrated dictionary

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,4 @@
 import express from "express"
-import axios from "axios"
-import url from "url"
 const app: express.Application = express()
 
 const bodyParser = require("body-parser");
@@ -8,6 +6,8 @@ import { getDictionaryEntries, getEntriesForSentence } from "./edict/repository"
 import { searchKanjiByRadicalDescriptions } from "./radical-search";
 import { DictionaryEntryInDb } from "./types";
 import { Db } from "mongodb";
+
+import handleIntegratedDictionary from "./integrated-dictionary/integrated-dictionary"
 
 let db: Db
 
@@ -56,45 +56,7 @@ app.get("/kanji-by-radical/:query", async (req: express.Request, res: express.Re
   res.json(output)
 })
 
-app.get("/integrated-dictionary/*", async (req: express.Request, res: express.Response) => 
-{
-  try
-  {
-    const targetUrlRaw = req.path.replace(/^\/integrated-dictionary\//, "")
-    console.log(req.path)
-    console.log(req.query)
-    const targetUrl = url.parse(targetUrlRaw)
-    const targetOrigin = targetUrl.protocol + "//" + targetUrl.host
-    const response = await axios.get(targetUrl.href, {
-      params: req.query
-    })
-
-    // to replace in all href and src:
-    // - urls starting with / (replace the trailing / with /integrated-dictionary/{targetOrigin})
-    // - urls starting with the target domain (replace it with /integrated-dictionary/{targetOrigin})
-
-    const contentType: string = response.headers["content-type"]
-    console.log(targetUrlRaw, contentType)
-    let output = ""
-    if (contentType.startsWith("text/html"))
-    {
-      output = response.data.replace(/(href|src)\s*=\s*(["'])\//ig,
-        "$1=$2/integrated-dictionary/" + targetOrigin + "/")
-
-      output = output.replace("。", "ぞ。")
-    }
-    else
-    {
-      output = response.data
-    }
-
-    res.type(contentType)
-    res.send(output)
-  } catch (error)
-  {
-    res.send(error)
-  }
-})
+app.get("/integrated-dictionary/*", handleIntegratedDictionary)
 
 export default app
 
---
+++ src/integrated-dictionary/integrated-dictionary.ts
@@ -0,0 +1,47 @@
+import express from "express"
+import axios from "axios"
+import url from "url"
+import { readFileSync } from "fs"
+
+const javascriptToInject = readFileSync("src/integrated-dictionary/javascript-to-inject.js", { encoding: "utf8" })
+
+
+export default async function handleIntegratedDictionary(req: express.Request, res: express.Response) 
+{
+  try
+  {
+    const targetUrlRaw = req.path.replace(/^\/integrated-dictionary\//, "")
+    console.log(req.path)
+    console.log(req.query)
+    const targetUrl = url.parse(targetUrlRaw)
+    const targetOrigin = targetUrl.protocol + "//" + targetUrl.host
+    const response = await axios.get(targetUrl.href, {
+      params: req.query
+    })
+
+    // to replace in all href and src:
+    // - urls starting with / (replace the trailing / with /integrated-dictionary/{targetOrigin})
+    // - urls starting with the target domain (replace it with /integrated-dictionary/{targetOrigin})
+
+    const contentType: string = response.headers["content-type"]
+    console.log(targetUrlRaw, contentType)
+    let output = ""
+    if (contentType.startsWith("text/html"))
+    {
+      output = response.data.replace(/(href|src)\s*=\s*(["'])\//ig,
+        "$1=$2/integrated-dictionary/" + targetOrigin + "/")
+
+      output = response.data.replace(/<head .*?>/i, "<head><script>" + javascriptToInject + "</script>")
+    }
+    else
+    {
+      output = response.data
+    }
+
+    res.type(contentType)
+    res.send(output)
+  } catch (error)
+  {
+    res.send(error)
+  }
+}
