90e84b6c3f249ebf1e633146383edffe36e799c7
iccanobif <iccanobif@protonmail.com>
2020-09-17 19:18:12 +0200

    ignore handakuten for ha-gyou

--- datasets/checkaccents.py
+++ datasets/checkaccents.py
@@ -4,14 +4,24 @@ import re
 f = open("compiled-accent-dictionary", "r", encoding="utf8")
 
 for line in f:
+    line = line.replace("カ゚", "ガ")
+    line = line.replace("キ゚", "ギ")
+    line = line.replace("ク゚", "グ")
+    line = line.replace("ケ゚", "ゲ")
+    line = line.replace("コ゚", "ゴ")
+
     j = json.loads(line)
     accents = j["contentLines"][1:-1]
     accents = [a for a in accents if not a.startswith("例文")]
     accents = [a for a in accents if not "][" in a]
 
+
     for accent in accents:
         if not re.match("^.*\[\d*\]$", accent):
             print(accent)
+
+        if not "]" in accent:
+          continue # it's a weird line like &emsp;（オ）
 
         number = int(re.search("\[\d*\]", accent)[0][1:-1])
         rawText = re.search(".*\[", accent)[0][:-1]
@@ -22,5 +32,5 @@ for line in f:
           reconstructedText = cleanText[:number] + "↓" + cleanText[number:]
         if rawText != reconstructedText:
           print(rawText, reconstructedText)
-          for c in rawText:
-            print(c)
+          # for c in rawText:
+          #   print(c)
