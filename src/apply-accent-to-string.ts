7c9c07a5514c8d3ba1de1b3b4a7877328e26de35
iccanobif <iccanobif@protonmail.com>
2020-09-22 23:19:09 +0200

    starting to implement applyAccentToString()

--- src/apply-accent-to-string.ts
+++ src/apply-accent-to-string.ts
@@ -1,7 +1,19 @@
 // accentedMora: 0 if word is flat (平板)
 //               N when the Nth mora is the last high-pitched one
 
-export function applyAccentToString(s: string, accentedMora: number): string
+export function applyAccentToString(inputText: string, accentedMora: number): string
 {
-  return ""
+  if (accentedMora === 0)
+    return inputText
+
+  let adjustedNumber = 0
+  for (let i = 0; i < accentedMora; i++)
+  {
+    adjustedNumber++
+    if (adjustedNumber < inputText.length
+      && ["ョ", "ャ", "ュ"].includes(inputText.charAt(adjustedNumber)))
+      adjustedNumber++
+  }
+
+  return inputText.substring(0, adjustedNumber) + "↓" + inputText.substring(adjustedNumber)
 }
