6762978db8c25e068d733e2d10378a762df981b1
iccanobif <iccanobif@protonmail.com>
2021-09-17 08:58:26 +0200

    useless commit

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,9 @@ let client: MongoClient = new MongoClient(environment.mongodbUrl,
 client.connect().then(() =>
 {
   setAppDatabase(client.db())
-  app.listen(environment.httpPort, "0.0.0.0")
+  app.listen(environment.httpPort, "0.0.0.0", () => {
+    console.log("app.listen() callback")
+  })
   log("Server running on http://localhost:" + environment.httpPort)
 })
   .catch(console.error)
