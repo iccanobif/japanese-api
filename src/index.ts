2d160ce1591003a1a18c77891224a6dcd9b09b03
iccanobif <iccanobif@protonmail.com>
2022-10-30 10:48:22 +0100

    accept connections only from localhost

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,9 @@ let client: MongoClient = new MongoClient(environment.mongodbUrl,
 client.connect().then(() =>
 {
   setAppDatabase(client.db())
-  app.listen(environment.httpPort, "0.0.0.0", () => {
+  // Accept connections only from localhost, since normally I want to
+  // connect to the node.js process via a reverse proxy, at least for SSL offloading.
+  app.listen(environment.httpPort, "localhost", () => {
     console.log("app.listen() callback")
   })
   log("Server running on http://localhost:" + environment.httpPort)
